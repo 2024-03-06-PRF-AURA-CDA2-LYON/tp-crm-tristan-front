903e6dd11b239edad6ef94aa7392afdf1158bf63
Titossaniti <tristan.erard@gmail.com>
2024-10-04 13:50:00 +0200

    Front working with API service to make requests

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Order} from "../../interface/interface";
-import {ApiService} from "../../service/api.service";
+import { Order, Customer } from "../../interface/interface";
+import { ApiService } from "../../service/api.service";
 
 @Component({
   selector: 'app-orders',
@@ -9,48 +9,59 @@ import {ApiService} from "../../service/api.service";
 })
 export class OrdersComponent implements OnInit {
   orders: Order[] = [];
-  newOrder: Order = {} as Order;
+  customers: Customer[] = [];
+  newOrder: Order = { customer: {} } as Order;
   selectedOrder: Order | null = null;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.getOrders();
+    this.getCustomers();
   }
 
-  getOrders():void {
+  getOrders(): void {
     this.apiService.getOrder().subscribe((data: Order[]) => {
       this.orders = data;
     });
   }
 
-  addOrder():void {
+  getCustomers(): void {
+    this.apiService.getCustomer().subscribe((data: Customer[]) => {
+      this.customers = data;
+    });
+  }
+
+  addOrder(): void {
     this.apiService.addOrder(this.newOrder).subscribe(() => {
       this.getOrders();
       this.newOrder = {} as Order;
     });
   }
 
-  selectOrderForUpdate(order: Order):void {
+  selectOrderForUpdate(order: Order): void {
     this.selectedOrder = { ...order };
+    if (!this.selectedOrder.customer) {
+      this.selectedOrder.customer = <Customer>{};
+    }
   }
 
-  updateOrder():void {
+  updateOrder(): void {
     if (this.selectedOrder) {
-      this.apiService.updateOrder(this.selectedOrder.id ,this.selectedOrder).subscribe(() => {
+      this.apiService.updateOrder(this.selectedOrder.id, this.selectedOrder).subscribe(() => {
         this.getOrders();
         this.selectedOrder = null;
       });
     }
   }
 
-  deleteOrder(id: number):void {
+  deleteOrder(id: number): void {
     this.apiService.deleteOrder(id).subscribe(() => {
       this.getOrders();
     });
   }
 
-  translateOrderState(state: string):string {
-    return state === 'CANCELLED' ? 'Annulée' : state === 'OPTION' ? 'Option' : 'Confirmée';
+  translateOrderState(state: string): string {
+    return state === 'CANCELED' ? 'Annulée' : state === 'OPTION' ? 'Option' : 'Confirmée';
   }
 }
